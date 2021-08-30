6d70ef71e3060d9c1f8b87e36a61484f9b5d8f7a
Ian Whitmore <ianwhitmor@gmail.com>
2021-08-30 16:58:21 -0500

    add main and horned beast files and content to files

---
+++ src/components/HornedBeast.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class HornedBeast extends React.Component {
+  render() {
+    return(
+      <div>
+      <h2>Horned Beast #1</h2>
+      <img title="The Great Horned Beast" src="" alt="" />
+      <p>This is a description of Beast #1. Look at how big and strong this beast is!</p>
+      </div>
+    )
+  }
+}
+
+export default HornedBeast;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,1 +1,14 @@
 import React from 'react';
+import HornedBeast from './HornedBeast';
+
+class Main extends React.Component {
+  render() {
+  return (
+    <div>
+      <HornedBeast />
+      <HornedBeast />
+    </div>
+  )};
+}
+
+export default Main;
