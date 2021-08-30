fb4c22c3db5661485c769fdce24ed83c62d2faf4
Ian Whitmore <ianwhitmor@gmail.com>
2021-08-30 17:21:26 -0500

    passed title, image, and description into hornedbeast component

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 class HornedBeast extends React.Component {
+  constructor(props){
+    super(props);
+  }
   render() {
     return(
       <div>
-      <h2>Horned Beast #1</h2>
-      <img title="The Great Horned Beast" src="" alt="" />
-      <p>This is a description of Beast #1. Look at how big and strong this beast is!</p>
+      <h2>{this.props.title}</h2>
+      <img title={this.props.title} src={this.props.src} alt={this.props.alt} />
+      <p>{this.props.description}</p>
       </div>
     )
   }
