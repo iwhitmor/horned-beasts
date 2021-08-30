1da96bb65af5d49021e10d0a43df3238429aac61
Ian Whitmore <ianwhitmor@gmail.com>
2021-08-30 16:41:20 -0500

    add footer to app.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,12 @@
 import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return(
+      <footer>
+        <h2>Created by Ian Whitmore</h2>
+      </footer>
+    )}
+  }
+
+export default Footer;
