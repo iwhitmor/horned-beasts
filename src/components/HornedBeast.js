8e4d7550a6cbcdf8b4ff4aedcb7f49c738b21ca9
Ian Whitmore <ianwhitmor@gmail.com>
2021-08-31 16:04:43 -0500

    add import data to main, create array and pass props from data array to page

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
 class HornedBeast extends React.Component {
-  // constructor(props){
-  //   super(props);
-  // }
+
   render() {
     return (
       <div>
         <h2>{this.props.title}</h2>
-        <img title={this.props.title} src={this.props.src} alt={this.props.alt} />
+        <img title={this.props.title} src={this.props.image} alt={this.props.alt} />
         <p>{this.props.description}</p>
       </div>
     )
