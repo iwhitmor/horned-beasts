d9ec677b0caff9c83221dcdeea40d2f3e69f6227
Ian Whitmore <ianwhitmor@gmail.com>
2021-09-02 10:54:44 -0500

    add cardcolumn

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ class Footer extends React.Component {
   render() {
     return(
       <footer>
-        <Navbar>
+        <Navbar bg="dark" variant="dark">
           <Navbar.Brand>Author: Ian Whitmore</Navbar.Brand>
         </Navbar>
       </footer>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
 import data from '../data/data.json'
+import CardColumns from 'react-bootstrap/Card';
 
 class Main extends React.Component {
   render() {
     return (
-      <div>
+      <CardColumns>
         {data.map(beast => (<HornedBeast title={beast.title} image={beast.image_url} description={beast.description} />))}
-      </div>
+      </CardColumns>
     )
   };
 }
